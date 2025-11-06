0e7acfe4504fb16e976df7ddb3e635adad63662e
Eneskoo3 <eneskoo33@gmail.com>
2025-11-06 17:25:06 +0100

    main methode wurde hinzugefügt

---
+++ Übung2_EK.cpp
@@ -0,0 +1,16 @@
+//Zum Kompilieren von C++ datein wird folgender 
+//Befehl in das Terminal eingegeben: 
+
+// g++ [Dateiname].cpp -o [Dateiname]
+
+//Zum Ausführen des Codes wird folgender Befehl 
+//Terminal eingegeben: 
+
+// ./[Dateiname]
+
+
+//Die Main methode:
+
+int main(){
+    return 0;
+}
